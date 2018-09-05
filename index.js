b37e7d78e6118930445c5aac3326a5d8a42a36bd
Milad Karbasizadeh <miladkdz@gmail.com>
2018-09-05 23:32:54 +0300

    Skeleton of what we are going to have.

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 #!/usr/bin/env node
 
 const chalk = require('chalk');
+const program = require('commander');
+const fs = require('fs');
+const path = require('path');
+const sharp = require('sharp');
 
-console.log(chalk.bold.red('Hi!!'));
+program.arguments('<file>')
+    .option('-w, --width <width>', 'Width of the result image', 800)
+    .action((file) => {      
+        const extension = path.extname(file);
+        const fileName = path.basename(file, extension);
+        if (fs.existsSync(file)) {
+            sharp(file)
+                .resize(program.width)
+                .toFile(`${fileName}-resized${extension}`);
+        } else {
+            console.log(chalk.bold.red('No such file.'));
+        }
+    })
+    .parse(process.argv);

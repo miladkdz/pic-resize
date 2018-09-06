6f8e779f53b3533d1580e2c9b40d81c27cfd52c8
Milad Karbasizadeh <miladkdz@gmail.com>
2018-09-06 09:15:54 +0300

    basic functionality done for now.

--- index.js
+++ index.js
@@ -8,13 +8,22 @@ const sharp = require('sharp');
 
 program.arguments('<file>')
     .option('-w, --width <width>', 'Width of the result image', 800)
-    .action((file) => {      
+    .action((file) => {
         const extension = path.extname(file);
         const fileName = path.basename(file, extension);
         if (fs.existsSync(file)) {
-            sharp(file)
-                .resize(program.width)
-                .toFile(`${fileName}-resized${extension}`);
+            const image = sharp(file);
+            image.metadata()
+                .then((metadata) => {
+                    image.resize(parseInt(program.width))
+                        .toFile(`${fileName}-resized${extension}`)
+                        .then(() => {
+                            console.log(chalk.bold.blue('Image resized successfuly.'));
+                        });
+                })
+                .catch((ex) => {
+                    console.log(chalk.bold.red(ex));
+                });                
         } else {
             console.log(chalk.bold.red('No such file.'));
         }
